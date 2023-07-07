e00288ab38b674018abcb053f206d17a4d041b04
zhensg123 <xyqblog@163.com>
2023-07-07 21:12:57 +0800

    '提交'

--- app.js
+++ app.js
@@ -13,19 +13,10 @@ const swaggerJSDoc = require('swagger-jsdoc')
 
 const cors = require('cors')
 
-// app.all('*', function (req, res, next) {
-//   // res.header("Access-Control-Allow-Origin", req.headers.origin);
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("Access-Control-Allow-Credentials","true");
-//   res.header("X-Powered-By",' 3.2.1')
-//   if(req.method === "OPTIONS") res.send(200);/*让options请求快速返回*/
-//   else  next();
-// })
 
-
-app.use(cors())
+app.use(cors({
+  credentials: true // 允许前端传递cookie
+}))
 
 var indexRouter = require('./routes/index');
 
